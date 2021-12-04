d185605673f6b9175b7733962740230d97ce2099
Michael Sims <michael.sims@trillianthealth.com>
2021-12-03 23:37:33 -0600

    Day 3 w/o bitwise operators

--- src/Day03.kt
+++ src/Day03.kt
@@ -4,22 +4,19 @@ import kotlin.math.min
 fun main() {
 
     fun part1(input: List<String>): Int {
-        val bitLength = input.first().length
-        val counts = IntArray(bitLength) { 0 }
-        for (byte in input.map { it.toBigInteger(2) }) {
-            for (i in 0 until bitLength) {
-                val mask = (1 shl i).toBigInteger()
-                if (byte and mask == mask) counts[i]++ else counts[i]--
+        val numLength = input.first().length
+        val counts = IntArray(numLength) { 0 }
+        for (number in input) {
+            for (i in 0 until numLength) {
+                if (number[i] == '1') counts[i]++ else counts[i]--
             }
         }
-        val gamma = counts
-            .reversed()
+        val gammaStr = counts
             .map { max(min(it, 1), 0) }
             .joinToString("") { it.toString() }
-            .toBigInteger(2)
-        val epsilon = gamma.inv() and ((1 shl bitLength) - 1).toBigInteger()
-
-        return (gamma * epsilon).toInt()
+        val gamma = gammaStr.toInt(2)
+        val epsilon = gammaStr.toCharArray().map { if (it == '0') '1' else '0' }.joinToString("").toInt(2)
+        return gamma * epsilon
     }
 
     fun part2(input: List<String>): Int {
