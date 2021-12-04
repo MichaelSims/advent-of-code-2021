7bc62f713e29fe33160c15e24223c45005d2fd7b
Michael Sims <michael.sims@trillianthealth.com>
2021-12-04 00:30:16 -0600

    Day 3 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -20,15 +20,25 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        tailrec fun findNumber(input: List<String>, filter: (Int, Int) -> Boolean, position: Int = 0): List<String> {
+            val ones = input.count { it[position].digitToInt() == 1 }
+            val zeroes = input.size - ones
+            val mostCommonOrOne = if (ones >= zeroes) 1 else 0
+            val filtered = input.filter { filter(it[position].digitToInt(), mostCommonOrOne) }
+            return if (filtered.size == 1) filtered else findNumber(filtered, filter, position + 1)
+        }
+
+        val oxygen = findNumber(input, { digit, mostCommonOrOne -> digit == mostCommonOrOne }).single().toInt(2)
+        val co2 = findNumber(input, { digit, mostCommonOrOne -> digit != mostCommonOrOne }).single().toInt(2)
+        return oxygen * co2
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 198)
-//    check(part2(testInput) == 900)
+    check(part2(testInput) == 230)
 
     val input = readInput("Day03")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
